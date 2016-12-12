cb0b754241635cfcd574a14cc9ccb353a846b3cd
Kit <feldspar.k@gmail.com>
2016-12-12 23:02:24 +0000

    more changes

--- bash/Junk/blah.sh
+++ bash/Junk/blah.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # This modified script creates an array from the ifconfig output, which contains
 # the names of the network interfaces. With those names, it is able to extract the
@@ -34,7 +34,7 @@ while [ $# -gt 0 ]; do
             -i )  # matches the int name argument
                   for x in "${intnames[@]}"
                   do
-                   if "$x" == "$2"
+                   if [[ "$x" == "$2" ]]
                    then
                      value="$2"
                      
@@ -42,7 +42,7 @@ while [ $# -gt 0 ]; do
                    fi
                   done
             ;;
-            -r | --route ) 
+            -r | --route ) #user must ask for the gateway route manually
             
             
             
@@ -70,13 +70,18 @@ done
 
 
 
-#while [ $intindex -lt ${#intnames[@]} ]; do
-#    ips[$intindex]=`ifconfig ${intnames[$intindex]} | grep 'inet addr' |
-#                                     sed -e 's/  *inet addr://'| sed -e 's/ .*//'`
-#                                     
-#    echo "Interface ${intnames[$intindex]} has address ${ips[$intindex]}"
-#    intfindex=$((intfindex + 1))
-#done
+
+
+while [[ $intindex -lt ${#intnames[@]} ]]; do
+    ips[$intindex]=`ifconfig ${intnames[$intindex]} | grep 'inet addr' |
+                                     sed -e 's/  *inet addr://'| sed -e 's/ .*//'`
+                                     
+    echo "Interface ${intnames[$intindex]} has address ${ips[$intindex]}"
+    intfindex=$((intfindex + 1))
+done
+
+
+
 
 
 #### The gateway needs to only show if the user has used the -r argument
