544e10ea460880af593ad007a3ae1fb19d7d788a
Kit <feldspar.k@gmail.com>
2016-12-12 23:31:15 +0000

    more changes

--- bash/lab4/showinterfaces.sh
+++ bash/lab4/showinterfaces.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
+
+
+# This script is horribly broken ;_;
 
 # This modified script creates an array from the ifconfig output, which contains
 # the names of the network interfaces. With those names, it is able to extract the
@@ -8,8 +11,11 @@
 
 declare -a ips
 declare -i intIndex ; intIndex=0
+declare value
 
-intnames=(`ifconfig |grep '^[a-zA-Z]'|awk '{print $1}'`)
+intnames=(`ifconfig |grep '^[a-zA-Z]'|awk '{print $1}'`) 
+
+
 
 ### Functions
 function expectedArguments {
@@ -22,42 +28,48 @@ function error-Message {
 # This section deals with any command-line input the user gives
 # Prompts for the user to specify an interfaces are implemented *further below* 
 
-while [ $# -gt  0 ]; do
-
-
+while [ $# -gt 0 ]; do
       case "$1" in
-      --help )  # matches the help argument
-            expectedArguments
-            exit 0
-      ;; 
-      -i )  # matches the count argument
-            if [[ "$2" =~ ^[${intnames[@]}$ ]]; then
-                  count=$2
-                  shift
-            else
-                  error-Message "The -c argument needs a number from 1 to 5"
-                  exit 2
-            fi
-      ;;
-      -s )  # matches the sides argument
-            if [[ "$2" =~ ^[1-9][0-9]*$ ]]; then
-            if [ "$2" -ge 4 -a "$2" -le 20 ]; then
-                  sides=$2
-                  shift
-            else
-                  error-Message "The -s argument needs a number from 4 to 20"
-                  exit 2
-            fi
-            else
-                  error-Message "The -s argument needs a number from 4 to 20"
-                  exit 2
-            fi
-      ;;
-      * )   # The catch-all case that returns the error function
-            expectedArguments
-            error-Message "I don't understand '$1'"
-            exit 2
-      ;;
+            --help )  # matches the help argument
+                  expectedArguments
+                  exit 0
+            ;; 
+            -i )  # matches the int name argument
+                  for x in "${intnames[@]}"
+                  do
+                   if [[ "$x" == "$2" ]]
+                   then
+                     value="$2"
+                     shift
+                   else
+                    error-Message "None of the interfaces match $value. "
+                     exit 2
+                   
+                   fi
+                  done
+            ;;
+            -r | --route ) #user must ask for the gateway route manually
+            
+            
+            
+#                  if [[ [${intnames[0]} =~ ^$2$ ]]; then
+#                        count="$2"
+#                        shift
+#                  else
+#                   if [[ [${intnames[1]} =~ ^$2$ ]]; then
+#                         count="$2"
+#                         shift
+#                   else
+#                    if [[ [${intnames[2]} =~ ^$2$ ]]; then
+#                          count="$2"
+#                          shift
+#                    else
+#                          error-Message "couldn't find $givenInt"
+#                          exit 2
+#                    fi
+#                   fi
+#                  fi
+            ;;
       esac
       shift
 done
@@ -66,27 +78,25 @@ done
 
 
 
-while [ $intfindex -lt ${#intnames[@]} ]; do
-    ips[$intfindex]=`ifconfig ${intnames[$intfindex]} | grep 'inet addr' |
+while [[ $intindex -lt ${#intnames[@]} ]]; do
+    ips[$intindex]=`ifconfig ${intnames[$intindex]} | grep 'inet addr' |
                                      sed -e 's/  *inet addr://'| sed -e 's/ .*//'`
                                      
-    echo "Interface ${intnames[$intfindex]} has address ${ips[$intfindex]}"
-    intfindex=$((intfindex + 1))
+    echo "Interface ${intnames[$intindex]} has address ${ips[$intindex]}"
+    intindex=$((intindex + 1))
 done
 
 
 
 
 
-intnames=(`ifconfig |grep '^[a-zA-Z]'|awk '{print $1}'`)
-ips[0]=`ifconfig ${intnames[0]} |grep 'inet addr' |sed -e 's/  *inet addr://'| sed -e 's/ .*//'`
-ips[1]=`ifconfig ${intnames[1]} |grep 'inet addr' |sed -e 's/  *inet addr://'| sed -e 's/ .*//'`
-
-gateway=`route -n |grep '^0.0.0.0 ' |awk '{print$2}'`
-
-cat <<BLARGH
-Interface ${intnames[0]}'s address is ${ips[0]}.
-Interface ${intnames[1]}'s address is ${ips[1]}.
-
-The default gateway is $gateway.
-BLARGH
+#### The gateway needs to only show if the user has used the -r argument
+#
+#gateway=`route -n |grep '^0.0.0.0 ' |awk '{print$2}'`
+#
+#cat <<BLARGH
+#Interface ${intnames[0]}'s address is ${ips[0]}.
+#Interface ${intnames[1]}'s address is ${ips[1]}.
+#
+#The default gateway is $gateway.
+#BLARGH
